063fd1ee2cedcf5f3ad55be101d534fdcf992119
KHUSHIBHANSALI04112000 <160408253+KHUSHIBHANSALI04112000@users.noreply.github.com>
2024-02-19 13:30:49 +0530

    Update shipments_controller.rb

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -3,3 +3,30 @@ class ShipmentsController < ApplicationController
     @shipments = Shipment.all
   end
 end
+class ShipmentsController < ApplicationController
+  before_action :get_company, :get_shipment, only: [:show]
+
+  def index
+    @shipments = Shipment.all
+  end
+
+  def show
+    grouped_shipment_items = @shipment.shipments_ordered_by_items_count(params[:items_order] || 'desc')
+    render json: { company: @company, shipment: @shipment, grouped_shipment_items: grouped_shipment_items }
+  end
+
+  private
+
+  def get_company
+    @company = Company.find(params[:company_id])
+  end
+
+  def get_shipment
+    if @company
+      @shipment = @company.shipments.find_by(id: params[:id])
+    else
+      @shipment = Shipment.find_by(id: params[:id])
+    end
+    render json: { error: 'Shipment not found' }, status: :not_found unless @shipment
+  end
+end
