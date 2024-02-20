9fcfd56baa0477ede9dd187048420515abe159b4
KHUSHIBHANSALI04112000 <160408253+KHUSHIBHANSALI04112000@users.noreply.github.com>
2024-02-20 22:32:34 +0530

    tracking

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -10,6 +10,38 @@ class ShipmentsController < ApplicationController
     render json: { company: @company, shipment: @shipment, grouped_shipment_items: grouped_shipment_items }
   end
 
+  def tracking
+    company_id = params[:company_id]
+    shipment_id = params[:id]
+    api_key = 'dummy_key'
+  
+    response = HTTParty.get(
+      "https://api.aftership.com/v4/trackings/#{shipment_id}",
+      headers: {
+        'Content-Type' => 'application/json',
+        'aftership-api-key' => api_key
+      }
+    )
+  
+    if response.code == 200
+      result = JSON.parse(response.body)
+      render json: {
+        status: result["data"]["tracking"]["tag"],
+        current_location: result["data"]["tracking"]["checkpoints"][-1]["location"],
+        last_checkpoint_message: result["data"]["tracking"]["checkpoints"][-1]["message"],
+        last_checkpoint_time: result["data"]["tracking"]["checkpoints"][-1]["checkpoint_time"]
+      }
+    else
+      render json: {
+        meta: {
+          code: 4004,
+          message: "Tracking does not exist.",
+          type: "BadRequest"
+        }
+      }, status: :not_found
+    end
+  end
+
   private
 
   def get_company
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :shipments, only: [:show]
   end
   resources :shipments, only: [:index, :show]
+  get '/companies/:company_id/shipments/:id/tracking', to: 'shipments#tracking'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
