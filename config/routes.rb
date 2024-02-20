7499f6d9f2d0851bc6f47736ee45b67dff2aead7
KHUSHIBHANSALI04112000 <160408253+KHUSHIBHANSALI04112000@users.noreply.github.com>
2024-02-20 22:27:55 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :companies, only: [:index, :show] do
+    resources :shipments, only: [:show]
+  end
+  resources :shipments, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
