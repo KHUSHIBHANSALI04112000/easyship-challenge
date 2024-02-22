45158ac8c005e080c1db3df286c5c41e8f2a54c9
KHUSHIBHANSALI04112000 <160408253+KHUSHIBHANSALI04112000@users.noreply.github.com>
2024-02-23 00:58:14 +0530

    review_comments_addressing

--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -1,35 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Shipment, type: :model do
+  describe "group_shipment_items_by_description_and_sort_order" do 
+    let(:company) { create(:company) }
+    let(:shipment) { create(:shipment, company: company) }
 
-  let(:company) { create(:company) }
-  let(:shipment) { create(:shipment, company: company) }
+    context 'fetch shipments ordered by count with respective description' do
+      before do
+        item_1 = create(:shipment_item, description: 'Iphone', shipment: shipment)
+        item_2 = create(:shipment_item, description: 'Macbook air', shipment: shipment)
+        item_3 = create(:shipment_item, description: 'Iphone', shipment: shipment)
+      end
 
-  context 'fetch shipments ordered by count with respective decription ' do
-    before do
-      item_1 = create(:shipment_item, description: 'Iphone', shipment: shipment)
-      item_2 = create(:shipment_item, description: 'Macbook air', shipment: shipment)
-      item_3 = create(:shipment_item, description: 'Iphone', shipment: shipment)
-    end
+      it 'should return shipments ordered by item count in descending order if order specified is descending' do
+        ordered_shipments = shipment.group_shipment_items_by_description_and_sort_order('desc')
 
-    it 'should return  shipments ordered by item count in descending order if order specified is descending' do
+        expect(ordered_shipments).to eq([
+          { description: 'Iphone', count: 2 },
+          { description: 'Macbook air', count: 1 },
+        ])
+      end
 
-      ordered_shipments = shipment.shipments_ordered_by_items_count('dsc')
+      it 'should return shipments ordered by item count in ascending order by default  if sort order is not specified' do
+        ordered_shipments = shipment.group_shipment_items_by_description_and_sort_order
 
-      expect(ordered_shipments).to eq([
-        { description: 'Iphone', count: 2 },
-        { description: 'Macbook air', count: 1 },
-      ])
-    end
-
-    it 'should return shipments ordered by item count in ascending order if order specified is ascending' do
-      ordered_shipments = shipment.shipments_ordered_by_items_count('asc')
-
-      expect(ordered_shipments).to eq([
-        { description: 'Macbook air', count: 1 },
-        { description: 'Iphone', count: 2 }
-      ])
+        expect(ordered_shipments).to eq([
+          { description: 'Macbook air', count: 1 },
+          { description: 'Iphone', count: 2 }
+        ])
+      end
     end
   end
 end
+
 
