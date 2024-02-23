13386358f11c7c6447768200eea956e3d388807c
KHUSHIBHANSALI04112000 <160408253+KHUSHIBHANSALI04112000@users.noreply.github.com>
2024-02-23 14:57:18 +0530

    Update shipments.rb

--- spec/factories/shipments.rb
+++ spec/factories/shipments.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     association :company, factory: :company
 
     after(:create) do |shipment|
-      create_list(:shipment_item, 3, description: "Iphone",shipment: shipment)
+      create_list(:shipment_item, 2, description: "Iphone",shipment: shipment)
+      create_list(:shipment_item, 1, description: "Macbook air",shipment: shipment)
     end
   end
 end
