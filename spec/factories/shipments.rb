10854ab008e9598054cae7a98dd1e60242964400
KHUSHIBHANSALI04112000 <160408253+KHUSHIBHANSALI04112000@users.noreply.github.com>
2024-02-24 11:00:31 +0530

    factories_random

--- spec/factories/shipments.rb
+++ spec/factories/shipments.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :shipment do
-    association :company, factory: :company
-
-    after(:create) do |shipment|
-      create_list(:shipment_item, 2, description: "Iphone",shipment: shipment)
-      create_list(:shipment_item, 1, description: "Macbook air",shipment: shipment)
-    end
+    association :company
   end
 end
